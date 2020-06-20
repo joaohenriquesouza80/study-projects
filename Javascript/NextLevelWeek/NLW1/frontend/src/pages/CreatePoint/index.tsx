e667fa65af169eaa94eb4a45f04536a402f8c690
João Henrique Souza <joaohenriquesouza80@gmail.com>
2020-06-20 09:20:50 -0300

    Next Level Week 1
    Dia 5
    Recursos avançados

--- Javascript/NextLevelWeek/NLW1/frontend/src/pages/CreatePoint/index.tsx
+++ Javascript/NextLevelWeek/NLW1/frontend/src/pages/CreatePoint/index.tsx
@@ -16,6 +16,8 @@ import "./styles.css";
 
 import logo from "../../assets/logo.svg";
 
+import DropZone from "../../components/DropZone";
+
 interface Item {
   id: number;
   title: string;
@@ -46,6 +48,7 @@ const CreatePoint = () => {
     0,
   ]);
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const [formData, setFormData] = useState({
     name: "",
@@ -143,16 +146,19 @@ const CreatePoint = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
-    };
+    const data = new FormData();
+
+    data.append("name", name);
+    data.append("email", email);
+    data.append("whatsapp", whatsapp);
+    data.append("uf", uf);
+    data.append("city", city);
+    data.append("latitude", String(latitude));
+    data.append("longitude", String(longitude));
+    data.append("items", items.join(","));
+    if (selectedFile) {
+      data.append("image", selectedFile);
+    }
 
     await api.post("points", data);
 
@@ -177,6 +183,8 @@ const CreatePoint = () => {
           Cadastro do <br />
           ponto de coleta
         </h1>
+
+        <DropZone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
